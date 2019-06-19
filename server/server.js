15d849445e64ed7d2da279cfcc954871d5b5412b
Jeff Day <jeffrey.day00@gmail.com>
2019-06-19 16:24:01 -0700

    Testing something.

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, '../client/public')));
 app.use('/stocks/', proxy('http://ec2-13-58-156-101.us-east-2.compute.amazonaws.com:3001/stocks/'));
 
 // marbo
-// app.use('/api/stocks/', proxy('http://ec2-18-222-193-177.us-east-2.compute.amazonaws.com:3111/api/stocks/'));
+app.use('/api/stocks/', proxy('http://ec2-18-222-193-177.us-east-2.compute.amazonaws.com:3111/api/stocks/'));
 
 // // matt
 // app.use('/api/users/', proxy('http://ec2-18-220-13-243.us-east-2.compute.amazonaws.com:3420/api/users/'));
