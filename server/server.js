e6bc438f4b518260fc5d062284b87ded92851c6c
Jeff Day <jeffrey.day00@gmail.com>
2019-06-19 11:58:09 -0700

    Adding proxies for other components.

--- server/server.js
+++ server/server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../client/public')));
 
-app.use('/stocks/', proxy('http://ec2-13-58-156-101.us-east-2.compute.amazonaws.com:3001/stocks'));
-
+app.use('/stocks/', proxy('http://ec2-13-58-156-101.us-east-2.compute.amazonaws.com:3001/stocks/'));
+app.use('/api/stocks/', proxy('http://ec2-18-222-193-177.us-east-2.compute.amazonaws.com:3111/api/stocks/'));
 
 
 app.listen(port, () => console.log('listening on: ', port))
